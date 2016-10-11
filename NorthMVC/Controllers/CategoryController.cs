f85c8711dc618d8d393085a91e601d9be28e41d2
mstzturk <mesutoztrk@msn.com>
2016-10-11 13:07:44 +0300

    first commit

---
+++ NorthMVC/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorthMVC.Models;
+
+namespace NorthMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View(new NorthwindEntities().Categories.ToList());
+        }
+
+        public ActionResult Detay(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("Index");
+            var kategori = new NorthwindEntities().Categories.Find(id.Value);
+            return View(kategori);
+        }
+        [HttpPost]
+        public ActionResult Duzenle(Category guncellenenKategori)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var guncellenecek = db.Categories.Find(guncellenenKategori.CategoryID);
+            guncellenecek.CategoryName = guncellenenKategori.CategoryName;
+            guncellenecek.Description = guncellenenKategori.Description;
+            db.SaveChanges();
+            return View("Index",db.Categories.ToList());
+        }
+
+        public ActionResult Yeni()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Yeni(Category cat)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            db.Categories.Add(cat);
+            db.SaveChanges();
+            return View("Index", db.Categories.ToList());
+        }
+    }
+}
---
+++ NorthMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NorthMVC.MvcApplication" Language="C#" %>
