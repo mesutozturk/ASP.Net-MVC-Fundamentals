0e650869c755fc0c626b3f684bbfd111807b1a58
mstzturk <mesutoztrk@msn.com>
2016-10-12 10:48:01 +0300

    product update page

--- NorthMVC/Controllers/ProductController.cs
+++ NorthMVC/Controllers/ProductController.cs
@@ -18,7 +18,37 @@ namespace NorthMVC.Controllers
         {
             if (id == null)
                 return RedirectToAction("Index");
-            return View(new NorthwindEntities().Products.Find(id));
+            NorthwindEntities db = new NorthwindEntities();
+            var kategoriler = new List<SelectListItem>();
+            kategoriler.Add(new SelectListItem()
+            {
+                Text = "Kategorisi Yok",
+                Value = "null"
+            });
+            db.Categories.ToList().ForEach(x =>
+                kategoriler.Add(new SelectListItem
+                {
+                    Text = x.CategoryName,
+                    Value = x.CategoryID.ToString()
+                })
+            );
+            ViewBag.Kategoriler = kategoriler;
+
+
+            return View(db.Products.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Duzenle(Product urun)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var guncellenecek = db.Products.Find(urun.ProductID);
+            guncellenecek.ProductName = urun.ProductName;
+            guncellenecek.UnitPrice = urun.UnitPrice;
+            guncellenecek.QuantityPerUnit = urun.QuantityPerUnit;
+            guncellenecek.UnitsInStock = urun.UnitsInStock;
+            guncellenecek.CategoryID = urun.CategoryID;
+            db.SaveChanges();
+            return RedirectToAction("Detay", new { id = urun.ProductID });
         }
     }
 }
