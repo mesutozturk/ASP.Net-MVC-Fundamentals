cab821f009fab62ac6c24bcc8457734fd9ae9a46
mstzturk <mesutoztrk@msn.com>
2016-10-12 09:52:35 +0300

    Add Product Page

---
+++ NorthMVC/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NorthMVC.Models;
+
+namespace NorthMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View(new NorthwindEntities().Products.ToList());
+        }
+    }
+}
